f7ff31c8d8d0704380f9977ed9922db687d67623
David Clement <david@zudu.co.uk>
2025-04-08 14:39:38 +0100

    desktopnav basics

---
+++ wp-content/themes/epass/Components/NavigationDesktop/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flynt\Components\NavigationJs;
+
+use Timber;
+use Flynt\Utils\Asset;
+use Flynt\FieldVariables;
+use Flynt\Utils\Options;
+
+add_action('init', function () {
+      
+  });
+
+add_filter('Flynt/addComponentData?name=NavigationDesktop', function ($data) {
+  $data['navOptions'] = Options::getGlobal('NavigationOptions');
+    $data['menu'] = Timber::get_menu('navigation_primary') ?? Timber::get_pages_menu();
+    // $data['menuSecondary'] = Timber::get_menu('navigation_secondary') ?? Timber::get_pages_menu();
+    $data['menuExtras'] = Timber::get_menu('navigation_primary_extras') ?? Timber::get_pages_menu();
+    $data['logo'] = wp_get_attachment_image( get_theme_mod( 'custom_logo' ) , 'full', '', ["class" => "logo"] );
+    //   var_dump($data['menu']);
+    return $data;
+});
