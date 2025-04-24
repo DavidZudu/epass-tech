348d913da600788a4fd963bba4fccdf612c69934
David Clement <david@zudu.co.uk>
2025-04-24 16:46:28 +0100

    hero header changes

--- wp-content/themes/epass/Components/LayoutHeroHeader/functions.php
+++ wp-content/themes/epass/Components/LayoutHeroHeader/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Flynt\Components\LayoutHeroHeader;
 
 use Flynt\FieldVariables;
@@ -7,7 +6,7 @@ use Flynt\FieldVariables;
 add_filter('Flynt/addComponentData?name=LayoutHeroHeader', function ($data) {
     if (isset($data['options']['sectionAnchor'])) {
         $data['options']['sectionAnchorLabel'] = $data['options']['sectionAnchor'];
-        $data['options']['sectionAnchor'] = preg_replace('/[^A-Za-z0-9]/', '-', strtolower($data['options']['sectionAnchor']));
+        $data['options']['sectionAnchor']      = preg_replace('/[^A-Za-z0-9]/', '-', strtolower($data['options']['sectionAnchor']));
     }
 
     return $data;
@@ -16,39 +15,71 @@ add_filter('Flynt/addComponentData?name=LayoutHeroHeader', function ($data) {
 function getACFLayout()
 {
     return [
-        'name' => 'LayoutHeroHeader',
-        'label' => __('Layout: Hero Header', 'flynt'),
+        'name'       => 'LayoutHeroHeader',
+        'label'      => __('Layout: Hero Header', 'flynt'),
         'sub_fields' => [
             [
-                'label' => __('Content', 'flynt'),
-                'name' => 'contentTab',
-                'type' => 'tab',
+                'label'     => __('Content', 'flynt'),
+                'name'      => 'contentTab',
+                'type'      => 'tab',
                 'placement' => 'top',
-                'endpoint' => 0,
+                'endpoint'  => 0,
             ],
             [
-            'label' => __('Page Title', 'flynt'),
-            'name' => 'pageTitle',
-            'type' => 'text',
+                'label'   => __('Pre-Title', 'flynt'),
+                'name'    => 'preTitle',
+                'type'    => 'text',
+                'wrapper' => [
+                    'width' => 30,
+                ],
             ],
             [
-            'label' => __('Content', 'flynt'),
-            'name' => 'content',
-            'type' => 'wysiwyg',
+                'label'   => __('Page Title', 'flynt'),
+                'name'    => 'pageTitle',
+                'type'    => 'text',
+                'wrapper' => [
+                    'width' => 70,
+                ],
+            ],
+            [
+                'label' => __('Content', 'flynt'),
+                'name'  => 'content',
+                'type'  => 'wysiwyg',
             ],
             FieldVariables\setCTAs(),
             [
-                'label' => __('Options', 'flynt'),
-                'name' => 'optionsTab',
-                'type' => 'tab',
-                'placement' => 'top',
-                'endpoint' => 0,
+                'label' => __('Add image', 'flynt'),
+                'name'  => 'image',
+                'type'  => 'image',
             ],
             [
-                'label' => '',
-                'name' => 'options',
-                'type' => 'group',
-                'layout' => 'row',
+            'label' => __('Logo label', 'flynt'),
+            'name' => 'logoLabel',
+            'type' => 'text',
+            'wrapper' => [
+                    'width' => 30,
+                ],
+            ],
+            [
+                'label' => __('Add Logos', 'flynt'),
+                'name'  => 'logos',
+                'type'  => 'gallery',
+                'wrapper' => [
+                    'width' => 70,
+                ],
+            ],
+            [
+                'label'     => __('Options', 'flynt'),
+                'name'      => 'optionsTab',
+                'type'      => 'tab',
+                'placement' => 'top',
+                'endpoint'  => 0,
+            ],
+            [
+                'label'      => '',
+                'name'       => 'options',
+                'type'       => 'group',
+                'layout'     => 'row',
                 'sub_fields' => [
                     FieldVariables\setBorders(),
                 ],
